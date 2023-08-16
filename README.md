d2bd98594b2d65ef9d43cbe6467d090827aeb8fd
suyog <suyog224356@gmail.com>
2023-08-16 20:36:12 +0545

    Polished the code and the README

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# yt_music_downloader
-This is a python script to install Youtube videos as music (mp3 files)
+# YouTube Music Downloader
+
+**Author:** Suyog Prasai  
+**GitHub:** [https://github.com/suyogprasai](https://github.com/suyogprasai)  
+**License:** MIT License  
+
+## Description
+
+This script allows you to download audio from YouTube videos and playlists. It provides a simple command-line interface to specify the location where you want to save the downloaded audio files and whether you want to download a single video or an entire playlist.
+
+## Features
+
+- Download audio from a single YouTube video.
+- Download audio from an entire YouTube playlist.
+- Option to specify the storage location for downloaded audio.
+- User-friendly command-line interface.
+
+## Dependencies
+
+- `pytube`: A lightweight, Pythonic library for downloading YouTube videos.
+- `pathlib`: A module providing classes for working with filesystem paths.
+- `time`: A module for measuring execution time and adding delays.
+
+For installation instructions, refer to the [dependencies.md](dependencies.md) file.
+
+## How to Use
+
+1. Clone or download this repository from GitHub.
+2. Open a terminal and navigate to the directory containing the script.
+3. Run the script using the following command:
+
+```shell
+python3 music_downloader.py
+```
+
+4. Follow the prompts to specify the download location and choose whether to download a video or a playlist.
+5. Enter the URL of the YouTube video or playlist you want to download audio from.
+6. The script will download the audio files to the specified location.
+
+## Example Usage
+
+1. Download audio from a single YouTube video:
+
+```shell
+Do you want to download a video or a playlist: [(V)/(P)] v
+Enter the URL of the music video you want to download: [YouTube Video URL]
+```
+
+2. Download audio from a YouTube playlist:
+
+```shell
+Do you want to download a video or a playlist: [(V)/(P)] p
+Enter the URL of the playlist you want to download: [YouTube Playlist URL]
+```
+
+## Notes
+
+- This script uses the `pytube` library to interact with YouTube. If you encounter any issues, make sure you have the latest version of the library installed.
+- Downloading copyrighted content without proper authorization may violate YouTube's terms of service and copyright laws. Please use this script responsibly.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- music_downloader.py
+++ music_downloader.py
@@ -1,101 +1,93 @@
 # Author: Suyog Prasai
-# Github: https://github.com/suyogprasai
-# Script: This script downloads music from videos
-# Dependencies: Refer to dependencies.md
+# GitHub: https://github.com/suyogprasai
+# Script: YouTube Music Downloader
+# Dependencies: Refer to README.md
 
 # Imports
-from pytube import YouTube
-from pytube import Playlist
+from pytube import YouTube, Playlist
 from pathlib import Path
 import time
 
-class Downloader: 
+class Downloader:
 
-    def __init__(self) -> None: 
+    def __init__(self):
         self.location = self.location_init()
-        print(f"[INFO] Location Set: [{self.location}] ")
+        print(f"[INFO] Location Set: [{self.location}]")
 
         self.choice = self.decision()
-        self.downloader = self.yt_downloader(self.location, (self.choice).upper())
+        self.downloader = self.yt_downloader(self.location, self.choice.upper())
 
-    def location_init(self) -> str: 
-        self.def_location = str(Path.home() / "music") 
-        while True: 
-            des = input(f"Do you want to use the default location [{self.def_location}]: [(y)\(n)] ")
-            des = des.upper()
-            if des == "Y" or des == "":
-                location = Path(self.def_location)
+    def location_init(self):
+        def_location = Path.home() / "music"
+        while True:
+            use_default = input(f"Do you want to use the default location [{def_location}]: [(y)/(n)] ").upper()
+            if use_default == "Y" or use_default == "":
+                location = def_location
                 break
-            elif des == "N":
+            elif use_default == "N":
                 location = input("Enter the path to your desired folder: ")
-                location = Path(location)
                 break
-            else: 
-                print("[ERROR] Intitializing the storage folder failed")
+            else:
+                print("[ERROR] Initializing the storage folder failed")
                 return 1
+
         try:
-            if Path.exists(location) == False: 
-                Path.mkdir(location)
-        except: 
+            location = Path(location)
+            if not location.exists():
+                location.mkdir(parents=True, exist_ok=True)
+        except:
             print("[ERROR] The path for the storage folder is incorrect")
         return str(location)
 
+    def download_audio(self, url, path):
+        try:
+            yt = YouTube(url)
+            audio_stream = yt.streams.filter(only_audio=True).first()
+            if audio_stream:
+                audio_stream.download(output_path=path)
+                print(f"[INFO] Audio [{yt.title}] downloaded successfully at [{path}].")
+            else:
+                print("[ERROR] No audio stream found for the given video")
+        except Exception as e:
+            print("[ERROR] An error occurred while downloading:", e)
+
     def d_video(self, path):
-        url = input("Enter the url of the music you want to download: ")
+        url = input("Enter the URL of the music video you want to download: ")
         start = time.time()
         self.download_audio(url, path)
-        end = time.time() 
-        total_time =  end - start
-        total_time = f"{total_time:.2f}"
-        print(f"\n[INFO] Audio has been downloaded at {path} in {total_time} seconds") 
-    
-    def d_playlist(self, path): 
-        playlist_url = input("Enter the url of the playlist that you wnat to download: ")
+        end = time.time()
+        total_time = end - start
+        print(f"\n[INFO] Audio has been downloaded at {path} in {total_time:.2f} seconds.")
+
+    def d_playlist(self, path):
+        playlist_url = input("Enter the URL of the playlist you want to download: ")
         plist = Playlist(playlist_url)
         playlist_name = plist.title
         pl_path = Path(path) / playlist_name
-        
-        if Path.exists(pl_path) == False: 
-            Path.mkdir(pl_path)
-        
+
+        if not pl_path.exists():
+            pl_path.mkdir(parents=True, exist_ok=True)
+
         start = time.time()
-        for url in plist.video_urls: 
-            self.download_audio(url, pl_path) 
+        for url in plist.video_urls:
+            self.download_audio(url, pl_path)
         total_songs = len(plist)
         end = time.time()
         total_time = end - start
-        total_time = f"{total_time:.2f}" 
-        print(f"\n[INFO] ({total_songs}) Audio(s) have been donwloaded at [{path}] from [{playlist_name}] in {total_time} seconds")
+        print(f"\n[INFO] ({total_songs}) Audio(s) have been downloaded at [{path}] from [{playlist_name}] in {total_time:.2f} seconds")
 
-    def download_audio(self, url, path): 
-        try: 
-            yt = YouTube(url)
-            audio_stream = yt.streams.filter(only_audio=True).first()
-            if audio_stream: 
-                audio_stream.download(output_path=path)
-                print(f"[INFO] Audio [{yt.title}] downloaded successfully at [{path}]. ")
-            else:
-                print("[ERROR] No audio stream found for the given video.")
-        except Exception as e:
-            print("[ERROR] An error occurred while downloading:", e)
-        
-    def yt_downloader(self, path, c): 
-
-        if c == "V": 
-            # Run download video function
+    def yt_downloader(self, path, c):
+        if c == "V":
             self.d_video(path)
-        elif c == "P": 
-            # Run download playlist function
+        elif c == "P":
             self.d_playlist(path)
 
     def decision(self) -> str:
-        while True: 
-            choice = input("Do you want to download a video or a playlist: [(V)\(P)] ")
-            choice = choice.upper()
-            if choice == "V" or choice == "P": 
+        while True:
+            choice = input("Do you want to download a video or a playlist: [(V)/(P)] ").upper()
+            if choice in ["V", "P"]:
                 return choice
-                break
-        return 1
-if __name__ == "__main__": 
-    program_instance = Downloader() 
-    pass 
+
+if __name__ == "__main__":
+    program_instance = Downloader()
+    pass
